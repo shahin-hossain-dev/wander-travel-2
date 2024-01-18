77905287b0f4a778592d974374aeaf76eb41ccc1
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-18 20:39:07 +0600

    toggle nav bar interactive with vanila JS

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('JS is Connect')
+
+const toggle = () => {
+
+    const navBar = document.getElementById('nav-items');
+    const displayWidth = document.documentElement.clientWidth;
+    const display = navBar.computedStyleMap().get('display').value;
+    console.log(displayWidth)
+
+    if (display === 'none') {
+        navBar.style.display = 'block';
+        window.addEventListener('resize', event => {
+            event.preventDefault();
+            const value = event.target.innerWidth;
+            if (value > 600) {
+                nav.navBar.display = 'flex'
+            }
+        })
+    }
+    else {
+        navBar.style.display = 'none';
+        window.addEventListener('resize', event => {
+            event.preventDefault();
+            const value = event.target.innerWidth;
+            if (value > 600) {
+                nav.navBar.display = 'flex'
+            }
+        })
+    }
+
+}
